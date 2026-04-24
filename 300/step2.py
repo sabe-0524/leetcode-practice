184a69da4a980800ed0fb6faf814a7068fa494a4
sabe-0524 <soichiro.0524@icloud.com>
2026-04-24 16:25:44 +0900

    300

---
+++ 300/step2.py
@@ -0,0 +1,14 @@
+from typing import List
+from bisect import bisect_left
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = []
+        for num in nums:
+            index = bisect_left(dp, num)
+            if index == len(dp):
+                dp.append(num)
+            else:
+                dp[index] = num
+        
+        return len(dp)
