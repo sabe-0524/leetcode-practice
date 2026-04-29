60304d2af94a24783c8766658b701d8ae21665e6
sabe-0524 <soichiro.0524@icloud.com>
2026-04-29 16:45:58 +0900

    33#

---
+++ 33/step1.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                if nums[left] <= target <= nums[mid]: 
+                    right = mid
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid
+        
+        return right if nums[right] == target else -1
