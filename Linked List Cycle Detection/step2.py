1ae04bdc5999b25d4443b4dad1c105134ca0a13d
sabe-0524 <soichiro.0524@icloud.com>
2026-09-24 23:29:42 +0900

    b

---
+++ Linked List Cycle Detection/step2.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        fast = head.next
+        slow = head
+        while fast and fast.next:
+            if fast == slow:
+                return True
+            slow = slow.next
+            fast = fast.next.next
+        
+        return False
