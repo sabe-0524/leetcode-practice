0d538f5d1cc072a376f7e38d407075f7f531dc59
sabe-0524 <soichiro.0524@icloud.com>
2026-09-26 10:54:32 +0900

    a

---
+++ Add Two Numbers/step1.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        current = dummy
+        
+        carry = 0
+        while l1 or l2:
+            num1 = l1.val if l1 else 0
+            num2 = l2.val if l2 else 0
+            
+            total = num1 + num2 + carry
+            carry = total // 10
+            remainder = total % 10
+            current.next = ListNode(remainder)
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        
+        if carry > 0:
+            current.next = ListNode(carry)
+        
+        return dummy.next
